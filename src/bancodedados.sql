2ddee85674675d79f4ca1d1da5cd2f2d35aafccc
unknown <gabitelhado@gmail.com>
2021-08-07 05:39:08 -0300

    criando banco de dados

---
+++ src/bancodedados.sql
@@ -0,0 +1,30 @@
+create database market_cubos;
+
+drop table if exists usuarios;
+
+create table usuarios(
+  id serial primary key,
+  nome text not null,
+  nome_loja text not null,
+  email text not null unique,
+  senha text not null
+);
+
+drop table if exists produtos;
+
+create table produtos(
+  id serial primary key,
+  usuario_id integer references usuarios(id) not null,
+  nome text not null,
+  estoque smallint not null,
+  categoria varchar(20),
+  preco integer not null,
+  descricao text,
+  imagem text not null
+);
+
+
+
+
+
+
