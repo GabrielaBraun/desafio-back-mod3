95fcba1b7837d9164a616f51cba855beeb4a1c69
unknown <gabitelhado@gmail.com>
2021-08-07 05:28:30 -0300

    endpoints

--- src/rotas.js
+++ src/rotas.js
@@ -1,5 +1,20 @@
 const express = require('express');
+const usuarios = require('./Controladores/usuarios');
+const produtos = require('./Controladores/produtos');
+
 const rotas = express();
+
+rotas.post('/cadastro', usuarios);
+rotas.post('login', usuarios);
+rotas.get('/perfil', usuarios);
+rotas.put('/perfil', usuarios);
+
+rotas.get('/produtos', produtos);
+rotas.get('/produtos/:id', produtos);
+rotas.post('/produtos', produtos);
+rotas.put('/produtos/:id', produtos);
+rotas.delete('/produtos/:id', produtos);
+
 
 
 module.exports = rotas;
