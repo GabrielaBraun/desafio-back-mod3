5cfc2936387696f6135f7afbe623dd01182cf2a4
unknown <gabitelhado@gmail.com>
2021-08-09 21:21:32 -0300

    cors para desfazer erro na requisicao

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 const express = require('express');
+const cors = require('cors');
 const rotas = require('./rotas');
 const app = express();
 
 app.use(express.json());
+app.use(cors());
 app.use(rotas);
 
-app.listen(3000);
+app.listen(3001);
